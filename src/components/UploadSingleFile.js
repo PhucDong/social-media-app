432a690e51cd7f5a5d0bd9585825f9d5fed73852
Phuc <dongvuminhphuc@gmail.com>
2024-03-09 13:24:04 +0700

    Update styles of DropZoneStyle

--- src/components/UploadSingleFile.js
+++ src/components/UploadSingleFile.js
@@ -1,20 +1,21 @@
-import styled from "@emotion/styled";
-import AddAPhotoRoundedIcon from "@mui/icons-material/AddAPhotoRounded";
-import { Box, Stack, Typography } from "@mui/material";
 import isString from "lodash/isString";
 import { useDropzone } from "react-dropzone";
+
+import { styled } from "@mui/material/styles";
+import { Box, Stack, Typography } from "@mui/material";
 import RejectionFiles from "./RejectionFiles";
+import AddAPhotoRoundedIcon from "@mui/icons-material/AddAPhotoRounded";
 
 const DropZoneStyle = styled("div")(({ theme }) => ({
   outline: "none",
   overflow: "hidden",
   position: "relative",
-  height: 300,
+  height: 200,
   padding: theme.spacing(3, 1),
   borderRadius: theme.shape.borderRadius,
   transition: theme.transitions.create("padding"),
-  backgroundColor: "#f4f6f8",
-  border: "1px dashed alpha('#919eab', 0.32)",
+  backgroundColor: "#F4F6F8",
+  border: `1px dashed alpha('#919EAB', 0.32)`,
   "&:hover": { opacity: 0.72, cursor: "pointer" },
 }));
 
@@ -25,7 +26,10 @@ function UploadSingleFile({ error = false, file, helperText, sx, ...other }) {
     isDragActive,
     isDragReject,
     fileRejections,
-  } = useDropzone({ multiple: false, ...other });
+  } = useDropzone({
+    multiple: false,
+    ...other,
+  });
 
   return (
     <Box sx={{ width: "100%", ...sx }}>
@@ -45,9 +49,8 @@ function UploadSingleFile({ error = false, file, helperText, sx, ...other }) {
       >
         <input {...getInputProps()} />
 
-        {/* Add explaination for the dropzone of image */}
         <Stack
-          directio="column"
+          direction="column"
           spacing={2}
           justifyContent="center"
           alignItems="center"
@@ -88,6 +91,8 @@ function UploadSingleFile({ error = false, file, helperText, sx, ...other }) {
       {fileRejections.length > 0 && (
         <RejectionFiles fileRejections={fileRejections} />
       )}
+
+      {helperText && helperText}
     </Box>
   );
 }
