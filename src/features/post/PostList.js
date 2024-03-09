11ab08ed21963834db9191372e564a32540f1b24
Phuc <dongvuminhphuc@gmail.com>
2024-03-09 19:05:47 +0700

    Add useEffect to render page after deleting a post

--- src/features/post/PostList.js
+++ src/features/post/PostList.js
@@ -8,16 +8,22 @@ import { Typography } from "@mui/material";
 function PostList({ userId }) {
   const [page, setPage] = useState(1);
   const dispatch = useDispatch();
-  const { postsById, currentPagePosts, totalPosts, isLoading } = useSelector(
-    (state) => state.post
-  );
+  const { postsById, currentPagePosts, totalPosts, isLoading, deletedPost } =
+    useSelector((state) => state.post);
   const posts = currentPagePosts.map((postId) => postsById[postId]);
 
   useEffect(() => {
     if (userId) {
       dispatch(getPosts({ userId, page }));
     }
+    console.log(21, "Posts are rendered");
   }, [dispatch, userId, page]);
+
+  useEffect(() => {
+    if (deletedPost) {
+      dispatch(getPosts({ userId, page }));
+    }
+  }, [deletedPost, dispatch, userId, page]);
 
   return (
     <>
