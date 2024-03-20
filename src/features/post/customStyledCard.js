b59735fc23f05d183cca39858bccd8b5ea25a7ba
Phuc <dongvuminhphuc@gmail.com>
2024-03-20 14:56:57 +0700

    Create custom styles for mod

---
+++ src/features/post/customStyledCard.js
@@ -0,0 +1,9 @@
+export const customStyledCard = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "500px",
+  maxWidth: "540px",
+  backgroundColor: "white",
+};
